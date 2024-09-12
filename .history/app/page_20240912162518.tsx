f04b47d923994c934a0c4eefa444c06286d05a00
devmskhan <71735576+devmskhan@users.noreply.github.com>
2024-09-12 16:37:37 +0100

    first

--- .history/app/page_20240912162518.tsx
+++ .history/app/page_20240912162518.tsx
@@ -92,7 +92,7 @@ export default function Home() {
 
 <footer className="bg-gray-900 text-white py-8">
 <div className="container mx-auto text-center">
-  <p>&copy; 2024 Hon. Aminu Galadima All rights reserved.</p>
+  <p>&copy; 2024 Hon. Aminu Galadanci All rights reserved.</p>
 </div>
 </footer>
     </div>
